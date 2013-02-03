180aba6f98b050c92f4ad5f176c0a709ff5d85ea
Guoxin Jin <jgmao@hotmail.com>
2013-02-03 17:51:40 -0600

    Initial Commit

---
+++ LinearProgramming/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LinearProgramming
+//
+//  Created by Guoxin Jin on 2/3/13.
+//  Copyright (c) 2013 Guoxin Jin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
